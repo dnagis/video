41ad13717c6b3e19b127fcc67182cc719a6f236e
dnagis <vincent.achard@gmail.com>
2022-06-21 19:02:24 +0200

    plot correct, mise à zero des premières frames qui ont un compte anormalement élevé

--- opencv/zero_plot.cpp
+++ opencv/zero_plot.cpp
@@ -12,6 +12,7 @@
 #include <opencv2/video.hpp>
 #include <opencv2/plot.hpp>
 
+
 using namespace cv;
 using namespace std;
 
@@ -39,12 +40,31 @@ int main(int argc, char* argv[])
 		zeroCount.push_back( countNonZero(fgMask) );
     }
     
+    //int max_count = 0;
+    //max_count = *max_element(zeroCount.begin(), zeroCount.end());
+    
+    //Sur le premier essai, les 4 premières frames sont > 300000, je les passe à 0 
+	for (int i = 0; i < 10; i++) {
+		zeroCount[i] = 0;
+	}
+    
     Ptr<plot::Plot2d> plot = plot::Plot2d::create( zeroCount );
     
     Mat plot_img;
+    
+    plot->setInvertOrientation( true );
+    plot->setShowText( false );   
+    
+    
     plot->render(plot_img);
+    
+
+    
     imshow( "Plot Title", plot_img );    
     waitKey();
+    
+    
+
 
     return 0;
 }
