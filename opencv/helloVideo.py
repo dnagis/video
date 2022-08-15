85817908e631c1e61a859c7e7225e753f2363bc7
dnagis <vincent.achard@gmail.com>
2022-08-15 08:08:51 +0200

    affichage frames

--- opencv/helloVideo.py
+++ opencv/helloVideo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#openCV: Hello world ouverture de video 
+#openCV: Hello world ouverture de video, affichage frames 
 #https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
 
 
@@ -8,10 +8,11 @@ import cv2 as cv
 
 
 
-cap = cv.VideoCapture('walkVauvert.mp4')
+cap = cv.VideoCapture('lunel_1.mp4')
 
 #calcul du temps (ms) à attendre entre chaque affichage de frame (cv.waitKey)
-fps=cap.get(cv.CAP_PROP_FPS) #https://docs.opencv.org/4.x/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+#Reference de tous les CAP_PROP_*: https://docs.opencv.org/4.x/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+fps=cap.get(cv.CAP_PROP_FPS) 
 wait_time=round(1000/fps)
 
 
@@ -22,8 +23,16 @@ while cap.isOpened():
         print("Can't receive frame (stream end?). Exiting ...")
         break
         
-    count=cv.CAP_PROP_FRAME_COUNT
-    print("CAP_PROP_=", count)
+
+
+    
+    ## [display_frame_number (trouvé dans tutorial_code/.../bg_sub.py)]
+    #get the frame number and write it on the current frame
+    pos_frames=str(cap.get(cv.CAP_PROP_POS_FRAMES))
+    #print("CAP_PROP_POS_MSEC=", str(cap.get(cv.CAP_PROP_POS_MSEC)))       
+    cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    cv.putText(frame, pos_frames, (15, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    ## [display_frame_number]
     
 
     
