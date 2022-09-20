c982d9acc5f8a79e1eada08e522267c776f45b71
dnagis <vincent.achard@gmail.com>
2022-09-20 13:18:00 +0200

    grosse avancée en compréhension grâce à np.shape()

--- opencv/hogsvm.py
+++ opencv/hogsvm.py
@@ -47,7 +47,7 @@ if img is None:
     raise Exception("we need the digits.png image from samples/data here !")
 
 #img: 2000(L)x1000(h) i.e. un array de 1000 fois 2000 pixels (np.ndim(img): 2, len(img)=1000, len(img[0])=2000)
-#print("np.shape(img):", np.shape(img)) # --> (1000, 2000) --> shape(): nombre d elements dans chaque dimension de la plus haute à la plus basse. Donc 1000 rox de 2000 px
+#print("np.shape(img):", np.shape(img)) # --> (1000, 2000) --> shape(): n elements dans chaque dimension de la plus haute à la plus basse. Donc 1000 rows de 2000 px
 
 cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
 
@@ -55,7 +55,7 @@ cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
 #puis np.hsplit(row,100) divise chaque row en 100 digits
 
 #print("np.ndim(cells):", np.ndim(cells)) # --> 4 dimensions
-#print("np.shape(cells):", np.shape(cells)) # --> (50, 100, 20, 20) shape(): nombre d elements dans chaque dimension de la plus haute à la plus basse 
+#print("np.shape(cells):", np.shape(cells)) #shape() donne la taille de chaque dimension, ici: (50, 100, 20, 20)
 #print("len(cells):", len(cells)) #50
 #print("taille de cells[0]:", len(cells[0])) #100
 #print("taille de cells[0][0]:", len(cells[0][0])) #20
@@ -73,7 +73,7 @@ cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
 #train_cells c'est la moitié gauche de digits.png, test_cells c'est la moitié droite
 
 train_cells = [ i[:50] for i in cells ] #50 premiers (0->50)
-#print("np.shape(train_cells):", np.shape(train_cells)) #(50, 50, 20, 20)
+#print("np.shape(train_cells):", np.shape(train_cells)) #shape() donne la taille de chaque dimension, ici: (50, 50, 20, 20)
 #il doit y avoir 50 iterations = iteration sur la première dimension (les rows). i[:50] retrieve les 50 premiers elements de chaque iteration
 
 test_cells = [ i[50:] for i in cells] #50 derniers (50->99)
@@ -88,18 +88,23 @@ test_cells = [ i[50:] for i in cells] #50 derniers (50->99)
 ######     Now training      ########################
 
 deskewed = [list(map(deskew,row)) for row in train_cells]
+#print("np.shape(deskewed):", np.shape(deskewed)) #shape() donne la taille de chaque dimension, ici: (50, 50, 20, 20)
 #print("taille de deskewed:",len(deskewed)) #50
 #print(train_cells[0][0])
 #print(deskewed[0][0])
 
 
 
-
 hogdata = [list(map(hog,row)) for row in deskewed]
-
+#print("np.shape(hogdata):", np.shape(hogdata)) #shape() donne la taille de chaque dimension, ici: (50, 50, 64)
 
 
 trainData = np.float32(hogdata).reshape(-1,64)
+#print("np.shape(trainData):", np.shape(trainData)) #shape() donne la taille de chaque dimension, ici: (2500, 64)
+#.reshape(-1,64) --> "Flattening an array" (convertir un array de n dimensions en un array de moindre n dimensions). Ici on converti du (50, 50, 64) en (2500, 64)
+#trainData est un array de 2500 x 64 (2500 digits de gauche. 50 rows de 50 digits). Chaque array de 64 est un Histogramme HOG de 64 bits d'un digit
+
+
 
 #Responses: 
 #np.arange(10) --> array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -108,8 +113,7 @@ trainData = np.float32(hogdata).reshape(-1,64)
 #donc responses est un array de 250 [0] suivi de 250 [1] etc ...
 responses = np.repeat(np.arange(10),250)[:,np.newaxis]
 
-#Avec ça j'ai en réponse le même taux de succès. Le modèle dit que les digits que je vois comme des 0 sont des9,
-#Et au test il teste si ce que je vois comme des 0 sont des 9
+#Avec np.flip j'ai en réponse le même taux de succès. Logique: il a été trained d'une certaine manière, il évalue de la même manière
 #responses = np.repeat(np.flip(np.arange(10),0),250)[:,np.newaxis]
 
 
