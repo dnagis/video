56f78254171ff353a68880b93de1ed81450abafb
dnagis <vincent.achard@gmail.com>
2022-09-30 09:39:44 +0200

    réduction des infos de detection hogsvm default people pour avoir début / fin de séquence

---
+++ opencv/hog_people_detect/data_segmentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#A partir des données de détection au format: 2 cols: timestamp [0 ou 1], arriver à "segmenter" les régions
+#qui contiennent une séquence interessante (où l'objet recherché est fréquemment détecté).
+
+
+
+import numpy as np
+  
+#Importer les donnees de detect.txt (générées par hog_pd_file) dans un array
+#deux cols dans detect.txt, je veux seulement la deuxième donc 1
+#https://www.geeksforgeeks.org/import-text-files-into-numpy-arrays/  
+data = np.loadtxt("detect.txt", usecols=1, dtype=int)
+
+print(data)
+
+print("np.shape(data):", np.shape(data)) #np.shape(data): (1838,)
+
+
+i=1 #step
+s_sub=5
+sub = data[i: i+ s_sub]
+print(sub)
+
+
