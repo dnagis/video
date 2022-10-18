b995a50c5cc7506b2473b86331a94095b5341a57
dnagis <vincent.achard@gmail.com>
2022-10-18 09:32:52 +0200

    début travail pour tests yolo moins fastidieux

---
+++ gst/pipeline.c
@@ -0,0 +1,94 @@
+/**
+ * gcc pipeline.c -o pipeline `pkg-config --cflags --libs gstreamer-1.0`
+ * gst-launch-1.0 filesrc location=file.mp4 ! decodebin ! videoconvert ! ximagesink
+ * 
+ * 
+ */
+
+
+#include <gst/gst.h>
+
+int
+main (int argc, char *argv[])
+{
+  GstElement *pipeline, *source, *sink, *decoder, *converter;
+  GstBus *bus;
+  GstMessage *msg;
+  GstStateChangeReturn ret;
+
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+  /* Create the elements */
+  //source = gst_element_factory_make ("videotestsrc", "source");
+  source = gst_element_factory_make ("filesrc", "source");
+  decoder = gst_element_factory_make ("decodebin", "decoder");
+  converter = gst_element_factory_make ("videoconvert", "converter");
+  sink = gst_element_factory_make ("ximagesink", "sink");
+
+  /* Create the empty pipeline */
+  pipeline = gst_pipeline_new ("test-pipeline");
+
+  if (!pipeline || !source || !decoder || !converter || !sink) {
+    g_printerr ("Not all elements could be created.\n");
+    return -1;
+  }
+
+  /* Build the pipeline */
+  gst_bin_add_many (GST_BIN (pipeline), source, decoder, converter, sink, NULL);
+  if (gst_element_link_many (source, decoder, converter, sink) != TRUE) {
+    g_printerr ("Elements could not be linked.\n");
+    gst_object_unref (pipeline);
+    return -1;
+  }
+
+  /* Modify the source's properties */
+  //g_object_set (source, "pattern", 0, NULL);
+  g_object_set (source, "location", argv[1], NULL);
+
+  /* Start playing */
+  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr ("Unable to set the pipeline to the playing state.\n");
+    gst_object_unref (pipeline);
+    return -1;
+  }
+
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  msg =
+      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+      GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+  /* Parse message */
+  if (msg != NULL) {
+    GError *err;
+    gchar *debug_info;
+
+    switch (GST_MESSAGE_TYPE (msg)) {
+      case GST_MESSAGE_ERROR:
+        gst_message_parse_error (msg, &err, &debug_info);
+        g_printerr ("Error received from element %s: %s\n",
+            GST_OBJECT_NAME (msg->src), err->message);
+        g_printerr ("Debugging information: %s\n",
+            debug_info ? debug_info : "none");
+        g_clear_error (&err);
+        g_free (debug_info);
+        break;
+      case GST_MESSAGE_EOS:
+        g_print ("End-Of-Stream reached.\n");
+        break;
+      default:
+        /* We should not reach here because we only asked for ERRORs and EOS */
+        g_printerr ("Unexpected message received.\n");
+        break;
+    }
+    gst_message_unref (msg);
+  }
+
+  /* Free resources */
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
