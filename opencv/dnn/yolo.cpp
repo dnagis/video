b46a67ab6b02d7d359aa77a74c3823ecdd0105ab
dnagis <vincent.achard@gmail.com>
2022-12-04 11:50:05 +0100

    travail sur les Mat en input pour vérifier si similaires

--- opencv/dnn/yolo.cpp
+++ opencv/dnn/yolo.cpp
@@ -74,9 +74,35 @@ int main( int argc, char** argv)
 	net.setPreferableBackend(DNN_BACKEND_OPENCV);
 	//net.setPreferableTarget(DNN_TARGET_CPU);
 	
-	Mat frame, blob;
-    frame = imread(argv[1]);
-    blobFromImage(frame, blob, 1/255, Size(inpWidth,inpHeight),Scalar(0,0,0), true, false);
+	Mat img, blob;
+    img = imread(argv[1], IMREAD_COLOR);
+    
+    if(img.empty())
+	    {
+	        cout << "Could not read the image: " << argv[1] << endl;
+	        return 1;
+	    }
+	    
+	if(img.type() == CV_8UC3)
+		{
+	        cout << "img type = CV_8UC3" << endl;
+	    }
+    
+    cout << "img.rows: " << img.rows << endl; 
+    cout << "img.cols: " << img.cols << endl; 
+    cout << "img.depth: " << img.depth() << endl; //img.depth()=0
+    cout << "img.channels: " << img.channels() << endl; //img.channels()=3
+    cout << "img.type: " << img.type() << endl; //img.type: 16     
+    
+    //avec 3 channels et type = 16 ce serait CV_8U selon https://stackoverflow.com/questions/10167534/how-to-find-out-what-type-of-a-mat-object-is-with-mattype-in-opencv
+    
+    //Comment accéder à une valeur particulière pour comparer avec Python
+    cout << "img.at(X,Y): " << img.at<uchar>(0,0) << endl; //
+    
+    //countNonZero() can handle only single channel images (https://answers.opencv.org/question/172784/count-non-zero-error/)
+    //cout << "countNonZeros: " << countNonZero(img) << endl; //countNonZero()=
+    
+    blobFromImage(img, blob, 1/255, Size(inpWidth,inpHeight),Scalar(0,0,0), true, false);
     net.setInput(blob);
     
     vector<Mat> outs;
