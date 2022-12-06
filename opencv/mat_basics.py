86bc9432f5e8ad6f66bc9a0ce279c499d2b5e702
dnagis <vincent.achard@gmail.com>
2022-12-06 09:52:20 +0100

    accès aux caractéristiques d'une image en Python

--- opencv/mat_basics.py
+++ opencv/mat_basics.py
@@ -11,14 +11,26 @@ import sys
 #FILE = 'digits.png'
 #img = cv.imread(cv.samples.findFile(FILE))
 
-img = cv.imread(sys.argv[1], IMREAD_COLOR)
+#https://docs.opencv.org/4.6.0/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
+img = cv.imread(sys.argv[1], cv.IMREAD_COLOR)
+
 
 if img is None:
     sys.exit("Could not read the image.")
+    
+#Pour récupérer les infos en Python, il semblerait qu'il soit nécessaire de considérer Mat
+#comme un numpy.ndarray, et récupérer les infos à partir de son shape    
+ 
+print('Image Dimension    : ',img.shape)
+print('Image Rows       : ',img.shape[0])
+print('Image Cols        : ',img.shape[1])
+print('Number of Channels : ',img.shape[2])
 
-#Accès aux valeurs individuelles
+print("type of data contained = ", img.dtype)
+
+#Accès à des valeurs individuelles
 #https://docs.opencv.org/4.6.0/d5/d98/tutorial_mat_operations.html
 value1 = img[0,0,0]
 value2 = img[0,0,1]
-print("value1 = ", value1)
-print("value2 = ", value2)
+print("une value 1 = ", value1)
+print("une value 2 = ", value2)
