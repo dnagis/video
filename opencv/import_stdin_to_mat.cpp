44d9308d787589ac5027ac74de370d2639c9b961
dnagis <vincent.achard@gmail.com>
2022-12-30 12:37:17 +0100

    boucle

--- opencv/import_stdin_to_mat.cpp
+++ opencv/import_stdin_to_mat.cpp
@@ -1,10 +1,11 @@
 /**
- * Import d'une raw frame from gstreamer dans une Mat opencv via stdin 
+ * Import de raw frames from gstreamer dans une Mat opencv via fdsink -> stdin 
  * 
  * g++ import_stdin_to_mat.cpp -o import_stdin_to_mat `pkg-config --cflags --libs opencv4`
  * 
- * gst-launch-1.0 --quiet videotestsrc num-buffers=1 ! video/x-raw,width=640,height=480,format=BGR,framerate=30/1 ! fdsink | ./import_stdin_to_mat
+ * gst-launch-1.0 --quiet videotestsrc ! clockoverlay ! video/x-raw,width=640,height=480,format=BGR,framerate=30/1 ! fdsink | ./import_stdin_to_mat
  * 
+ * kill -s SIGINT `pidof gst-launch-1.0`
  * 
  * 
 */
@@ -28,21 +29,38 @@ int main()
 		int framesize = IMAGE_WIDTH * IMAGE_HEIGHT * IMAGE_DEPTH;
 		Mat img;
 
-		//Memory allocation for image data buffer.
-		imagebuffer = (char*) malloc (sizeof(char) * framesize);
+
 		
-		//Rx depuis stdin ref: https://cplusplus.com/reference/istream/istream/read/
-		cin.read(imagebuffer, framesize);
+		//Création d'une Mat
+		//img.create(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3);
 		
-		img.create(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3);
 		
-		memcpy(img.data, imagebuffer, framesize);
+		while (!cin.eof()) {
+			//cout << "Début loop read buffer" << endl;
+			
+			//Memory allocation for image data buffer.
+			imagebuffer = (char*) malloc (sizeof(char) * framesize);
+			
+			//Rx depuis stdin ref: https://cplusplus.com/reference/istream/istream/read/
+			cin.read(imagebuffer, framesize);
+			//memcpy(img.data, imagebuffer, framesize);
+			
+			free(imagebuffer);
+			
+			
+		}
 		
-		free(imagebuffer);
 		
-		imshow("Display window", img);
-		int k = waitKey(0); // Wait for a keystroke in the window
 		
+		
+		
+		
+		
+		//Affichage
+		//imshow("Display window", img);
+		//int k = waitKey(0); // Wait for a keystroke in the window
+		
+		cout << "Fin du programme" << endl;
 		return 0;
 
 }
