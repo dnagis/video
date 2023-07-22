6283294faeacbc8f00629e04fd86fd2d94436780
dnagis <vincent.achard@gmail.com>
2023-07-22 12:48:04 +0200

    travail sur vecteurs de détections

--- opencv/detect_to_cut/parse.cpp
+++ opencv/detect_to_cut/parse.cpp
@@ -7,7 +7,9 @@
 std::vector<int> frames_vector{};
 std::vector<int> detect_vector{};
 
-int N_TAIL = 30;
+int N_TAIL = 30; //n detections à analyser à la fin (tail)
+int MAX_GAP = 3; //longueur d'une sequence de non detection qui signe la fin d'une séquence et pas un gap de detect
+int MIN_SIZE = 5; //taille en dessous de laquelle on estime que séquence pas assez longue
 
 //Overloading << Operator juste pour imprimer les vectors avec un simple std::cout << detect_vector;
 //https://www.techiedelight.com/print-vector-cpp/
@@ -17,6 +19,44 @@ std::ostream &operator<<(std::ostream &os, const std::vector<int> &input)
         os << i << " ";
     }
     return os;
+}
+
+
+//dans un vector de detections chercher après l'index start la première occurence d'une série de 
+//n=MAX_GAP 0
+//https://www.educative.io/answers/how-to-use-the-search-function-in-cpp
+int findEnd(std::vector<int> detections, int start) {
+	
+	std::vector<int> n_zeros(MAX_GAP, 0); //un vector de MAX_GAP zeros
+	
+	auto it = search(detections.begin() + start, detections.end(), n_zeros.begin(), n_zeros.end());
+	
+	if (it == detections.end()) 
+		{
+			//std::cout << "End not found" << std::endl;
+			return -1;			
+		} else
+		{
+			auto index = std::distance(detections.begin(), it);
+			//std::cout << "End found at position " << index << std::endl;
+			//std::cout << "End found at position " << (it-detections.begin()) << std::endl;
+			return index;
+		}
+}
+
+//dans un vector de detections chercher la première occurence de 1, renvoyer index, sinon -1
+int findStart(std::vector<int> detections) {
+	
+	auto it = std::find(detections.begin(), detections.end(), 1);
+	
+	if (it == detections.end())
+		{
+			return -1; //not found 
+		} else
+		{
+			auto index = std::distance(detections.begin(), it);
+			return index;
+		}
 }
 
 
@@ -94,7 +134,18 @@ int main (int argc, char *argv[]) {
   std::cout << "Tail_detect size:" << tail_detect.size() << std::endl;
   std::cout << tail_detect << std::endl;
 
+  int start_index = findStart(tail_detect);
   
+  std::cout << "Start:" << start_index << std::endl;
+  
+  if (start_index != -1) {
+	int end_index = findEnd(tail_detect, start_index);  
+	std::cout << "End: " << end_index << std::endl;
+	
+	
+	
+	
+	}
   
   return 0;
 }
