7fae70484845053009c1c943f8d4b3ff875c71e2
dnagis <vincent.achard@gmail.com>
2023-07-22 09:29:26 +0200

    récupérer la fin du vector de detections

--- opencv/detect_to_cut/parse.cpp
+++ opencv/detect_to_cut/parse.cpp
@@ -7,7 +7,9 @@
 std::vector<int> frames_vector{};
 std::vector<int> detect_vector{};
 
-//Overloading << Operator juste pour imprimer les vectors à la fin
+int N_TAIL = 30;
+
+//Overloading << Operator juste pour imprimer les vectors avec un simple std::cout << detect_vector;
 //https://www.techiedelight.com/print-vector-cpp/
 std::ostream &operator<<(std::ostream &os, const std::vector<int> &input)
 {
@@ -18,6 +20,28 @@ std::ostream &operator<<(std::ostream &os, const std::vector<int> &input)
 }
 
 
+//obtenir un subvector de la fin du vecteur de detections (style tail)
+std::vector<int> getTail(std::vector<int> detections) {
+	std::cout << detections << std::endl;
+	
+	
+	int totDetectSize = detections.size();
+	
+	//construction d'un subvector
+	std::vector<int>::const_iterator first = detections.end() - N_TAIL;
+	std::vector<int>::const_iterator end = detections.end();
+	
+	std::vector<int> tail_detect(first, end);
+	
+	//std::cout << " taille de tail_detect:" << tail_detect.size() << std::endl;
+	//std::cout << tail_detect << std::endl;
+	
+	return tail_detect;
+	
+}
+
+//Ouvre le fichier /root/results.txt, getline sur chaque ligne, regex qq chose type 634 0, 
+//Je ne peux pas renvoyer deux vectors pour frames et detection donc wide scope variables
 void parseResults() {
 	
 	
@@ -28,36 +52,26 @@ void parseResults() {
 	//return 1;
   }
   
-  //regex pour les lignes frame number, un espace, puis 0 ou 1
+  //regex pour les lignes: frame number ([0-9]*), puis un espace, puis 0 ou 1 ([01])
   //https://en.cppreference.com/w/cpp/regex
   std::regex ma_regex("^[0-9]* [01]");
   
   std::string line;
 	while (std::getline(results_file, line)) {
-	
-	
 	if (std::regex_search(line, ma_regex)) {
         //std::cout << "regex match\n";
         //std::cout << "The size of line is " << line.size() << " bytes.\n";
         std::string frame = line.substr (0,line.size()-2); 
         std::string detect = line.substr (line.size()-1,1);
-        std::cout << line << " frame=" << frame << " detect=" << detect << std::endl;
+        //std::cout << line << " frame=" << frame << " detect=" << detect << std::endl;
         
         int frame_int=atoi(frame.c_str()); //atoi(): You have to call the c_str() function of the string class when passing it to standard C functions. It returns you a const char*
         frames_vector.push_back(frame_int);    
               
         int detect_int=atoi(detect.c_str()); //atoi(): You have to call the c_str() function of the string class when passing it to standard C functions. It returns you a const char*
         detect_vector.push_back(detect_int);
-        
-      
-        
-        
-        
     }
-	
-	
   }
-  
   results_file.close();
 	
 }
@@ -68,10 +82,19 @@ void parseResults() {
 
 int main (int argc, char *argv[]) {
 
-  parseResults();
+  parseResults(); //doit remplir detect_vector
   
-  std::cout << frames_vector;
-  std::cout << detect_vector;
+  //std::cout << frames_vector;
+  //std::cout << detect_vector;
+  
+  //std::cout << " taille de detect_vector après parseResults:" << detect_vector.size() << std::endl;
+  
+  std::vector<int> tail_detect = getTail(detect_vector);
+  
+  std::cout << "Tail_detect size:" << tail_detect.size() << std::endl;
+  std::cout << tail_detect << std::endl;
+
+  
   
   return 0;
 }
