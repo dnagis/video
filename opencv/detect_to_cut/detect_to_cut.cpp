762cf2c1ecf534176724fa914db5109756f01daf
dnagis <vincent.achard@gmail.com>
2023-07-23 17:44:47 +0200

    loop avec goto pour skipper une iteration sur condition

---
+++ opencv/detect_to_cut/detect_to_cut.cpp
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <iostream>
+
+int INTERVAL_S = 2;
+
+int main (int argc, char *argv[]) {
+	
+	
+	while (1) {
+	std::cout << "Debut loop" << std::endl;
+	
+	
+	//goto end;
+	
+	std::cout << "Milieu loop" << std::endl;
+	
+	
+	
+	
+	end:
+	sleep(INTERVAL_S); //#include <unistd.h>
+	}
+	
+	
+return 0;
+}
